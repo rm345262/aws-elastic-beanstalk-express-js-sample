1eafd849f89bef17bbb8d11efd9fca756f0942d5
rm345262 <120472156+rm345262@users.noreply.github.com>
2022-12-22 10:59:33 -0300

    Update app.js

--- app.js
+++ app.js
@@ -3,8 +3,8 @@ const app = express();
 const port = 8080;
 
 #app.get('/', (req, res) => res.send('Hello rm345262 Equipe Jato'));
-git add app.js
-git commit -m "Full pipeline teste"
+git add app.js;
+git commit -m "Full pipeline teste";
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
